dfed4191059f09bf4907a9b9bc7fa0418e79ca40
Guillaume cornet <Guillaume-cornet07@hotmail.fr>
2023-02-07 15:49:33 +0100

    feat(router): add check health

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -1,7 +1,5 @@
 package server
 
-import "github.com/vinicel/listEvent/auth"
-
 func (s *Server) InitRoutes() {
-	s.Router.HandleFunc("/health", auth.Ping)
+	s.Router.HandleFunc("/health", healthCheck)
 }
